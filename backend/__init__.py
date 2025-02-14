5f68c09b912b55ec685e07931d63be167e9dbdeb
faridrazza <mdfarid.razza786@gmail.com>
2025-02-14 15:37:31 +0530

    just minor changes

--- backend/__init__.py
+++
@@ -1,8 +0,0 @@
-from backend.config import Config
-from backend.database.vector_store import VectorStore
-from backend.services.chat_service import ChatService
-from backend.services.report_service import ReportService
-from backend.services.market_service import MarketService
-from backend.models.portfolio import Portfolio
-
-__version__ = "1.0.0" 
